3ee7439ef190be7372cac5501ff8226fc368b4b0
jonathan Haas <haasj@upenn.edu>
2021-07-16 09:59:53 -0400

    Fix Upstream 2.0 Defaults
    Fixed masthead and site name
    
    UPSTREAM-181

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -35,5 +35,5 @@ if (file_exists($local_settings)) {
 }
 
 $config_directories['sync'] = __DIR__ . "/config";
-$config['system.site']['name'] = 'President\'s Engagement Prices';
+$config['system.site']['name'] = 'Change to Site Name';
 $config['system.site']['page']['front'] = '/home';
