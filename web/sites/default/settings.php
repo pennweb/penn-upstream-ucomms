b7b5fa091cceac0b567feab665f9c227da54f1e8
Jonathan haas <haasj@upenn.edu>
2021-10-12 16:32:33 -0400

    Drupal 9 Upgrade
    Upgraded to Drupal 9
    
    UPSTREAM-213

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -20,13 +20,6 @@ include __DIR__ . "/settings.pantheon.php";
 $settings['simplesamlphp_dir'] = $_ENV['HOME'] .'/code/private/simplesamlphp';
 
 /**
- * Place the config directory outside of the Drupal root.
- */
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
-);
-
-/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
