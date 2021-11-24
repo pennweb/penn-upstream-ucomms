7b3fc61868d4a4f6ef687ce6277e70a213f13d99
jonathan Haas <haasj@upenn.edu>
2021-11-24 11:24:35 -0500

    Fix Redirects Include Drush Error
    added conditional
    
    UPSTREAM-236

--- web/sites/default/default.settings.redirects.php
+++ web/sites/default/default.settings.redirects.php
@@ -1,9 +1,11 @@
 <?php 
 
-$request = rtrim(urldecode($_SERVER['REQUEST_URI']), '/');
-switch ($request) {
-  case '/old-url':
-    header('HTTP/1.0 301 Moved Permanently');
-    header('Location: /new-url');
-    exit();
+if (isset($_SERVER['REQUEST_URI'])){
+  $request = rtrim(urldecode($_SERVER['REQUEST_URI']), '/');
+  switch ($request) {
+    case '/old-url':
+      header('HTTP/1.0 301 Moved Permanently');
+      header('Location: /new-url');
+      exit();
+  }
 }
