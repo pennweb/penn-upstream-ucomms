d0439b8f0c925cdf8dd21b046e01eb9bc2f0ecd2
jonathan Haas <haasj@upenn.edu>
2022-06-02 10:51:28 -0400

    Fix default.settings.site
    Added <?php and search label
    
    UPSTREAM-286

--- web/sites/default/default.settings.site.php
+++ web/sites/default/default.settings.site.php
@@ -1,3 +1,6 @@
+<?php
+
 $config_directories['sync'] = __DIR__ . "/config";
 $config['system.site']['name'] = 'Change to Site Name';
 $config['system.site']['page']['front'] = '/home';
+$config['penn_search.settings']['penn_search_label'] ='Change to Search Label';
